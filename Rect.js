f3f87594cff3f4f64beadd8e1dbed82ddd10fc26
Kumail2008 <69812462+Kumail2008@users.noreply.github.com>
2020-08-29 15:53:49 -0400

    Add files via upload

---
+++ Rect.js
@@ -0,0 +1,14 @@
+class Rect {
+    constructor(x, y, width, height) {
+      var options = {
+          'restitution':0.8,
+          'friction':0.3,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){}
